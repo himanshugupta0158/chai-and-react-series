6779cb34e150e27ded6f9713dd06183839536b12
himanshugupta0158 <himanshugupta0158@gmail.com>
2024-05-01 14:17:20 +0530

    tailwind config for mode changes

--- 09themeSwitcher/tailwind.config.js
+++ 09themeSwitcher/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // this darkMode will enable mode switching in tailwind css
   darkMode: "class",
   theme: {
     extend: {},
