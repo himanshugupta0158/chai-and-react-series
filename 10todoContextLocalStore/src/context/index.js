737a7b9603fc1eea4fbde62c1743dd2803e040c7
himanshugupta0158 <himanshugupta0158@gmail.com>
2024-05-01 21:21:03 +0530

    learned using context with localstorage

---
+++ 10todoContextLocalStore/src/context/index.js
@@ -0,0 +1,4 @@
+// Index file is mainly used to get all imports from this current folder and get all these imported funxtion, values , etc form index
+// js file itself
+
+export { TodoContext, TodoProvider, useTodo } from "./TodoContext";
